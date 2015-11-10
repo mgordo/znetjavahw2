ffdd2b8f48d20527580b4cb451dd7f4271af00f3
trabladorr <trbldrr@gmail.com>
2015-11-10 21:07:13 +0100

    MessageSender: added sending a move to all peers, and extra helper method to send using the custom address string of peer

--- homework1GordoTatsis/src/hw1opt2/MessageSender.java
+++ homework1GordoTatsis/src/hw1opt2/MessageSender.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -21,12 +22,32 @@ public class MessageSender {
 	private static final Executor sender = new ThreadPoolExecutor(1, 5, 1, TimeUnit.SECONDS,  new LinkedBlockingQueue<Runnable>());
 	private static final ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1);
 	
-	public static void sendMessage(final Message m, final String to, final InetAddress ip, final int port){
+	public static void sendMessageToAllPeers(final Message m){
+		Map<String,String> addressMap = PRSGame.getInstance().getPeerObject().getAddress_list();
+		Set<String> recipients = addressMap.keySet();
+		
+		for (String to:recipients){
+			sendMessage(m, to, addressMap.get(to));
+		}
+	}
+	
+	public static void sendMessage(final Message m, final String to, final String address){
+		try {
+			InetAddress ip = InetAddress.getByName(address.split(" ")[0]);
+			int port = Integer.valueOf(address.split(" ")[1]);
+			sendMessage( m, to, ip, port);
+		}
+		catch (Exception e) {
+			PRSGame.getInstance().removePeer(to);
+		}
+	}
+	
+	public static void sendMessage(final Message m, final String to, final InetAddress address, final int port){
 		sender.execute(new Runnable() {
 			public void run() {
 				Socket socket = null;
 				try {
-					socket = new Socket(ip, port);
+					socket = new Socket(address, port);
 					PrintWriter wr = new PrintWriter(socket.getOutputStream());
 					wr.write(m.toString());
 					wr.flush();
@@ -73,7 +94,9 @@ public class MessageSender {
 			this.port = port;
 		}
 		public void run() {
-			sendMessage(new Message(), to, ip, port);//TODO: keep alive message
+			Message m = new Message(PRSGame.getInstance().getName());
+			m.makeAliveMessage();
+			sendMessage(m, to, ip, port);//TODO: keep alive message
 			futureHeartbeats.put(to, heartbeatScheduler.schedule(new HeartBeatRunnable(to, ip, port), heartbeatDelay, TimeUnit.MILLISECONDS));
 		}
 	}
